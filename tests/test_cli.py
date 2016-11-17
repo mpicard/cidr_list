d8de79772032c9b2464d72e56bf14685ed37a5be
Martin Picard <map@cix.ie>
2016-11-17 15:02:00 +0000

    Fixed MANIFEST.in

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,15 +5,18 @@ from netaddr import IPNetwork
 from click.testing import CliRunner
 from cidr_list import cli
 
+
 @pytest.fixture
 def runner():
     return CliRunner()
+
 
 @pytest.fixture
 def mock_open(request):
     m = mock.patch('__builtin__.open', mock.mock_open())
     m.start()
     request.addfinalizer(m.stop)
+
 
 def test_cli(runner):
     arg = '10.10.10.0/28'
@@ -23,6 +26,7 @@ def test_cli(runner):
     assert not result.exception
     assert result.output.strip() == ','.join([str(i) for i in ip])
 
+
 def test_cli_with_seperator(runner):
     arg = '10.10.10.0/30'
     ip = IPNetwork(arg)
@@ -30,6 +34,7 @@ def test_cli_with_seperator(runner):
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == ':'.join([str(i) for i in ip])
+
 
 def test_cli_with_range(runner):
     arg = '10.10.10.0/30'
@@ -42,9 +47,7 @@ def test_cli_with_range(runner):
 
 def test_cli_with_file(runner, mock_open):
     arg = '10.10.10.0/30'
-    ip = IPNetwork(arg)
     result = runner.invoke(cli.main, [arg, '-f output.txt'])
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == ""
-
